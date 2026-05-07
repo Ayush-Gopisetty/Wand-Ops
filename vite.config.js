74fc43b210b52611f74acb9638758be3dc007d70
Ayush Gopisetty <ayush.gopisetty@gmail.com>
2026-05-07 18:12:05 -0500

    Fixed deployment issue

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // index.html lives at root; /public holds static assets (styles.css etc.)
   build: {
     target: 'esnext',
     outDir: 'dist',
